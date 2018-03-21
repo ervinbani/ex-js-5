914737213c06b5e673042c65bf1fd5ca363377a4
ervinbani <bani.ervin@gmail.com>
2018-03-21 11:10:07 +0100

    finale

--- main.js
+++ main.js
@@ -8,50 +8,46 @@ Quando si ferma,il programma deve mostrare il punteggio del computer e il
 
 //Il computer sceglie casualmente un numero e lo memorizza
 var pcNumber = Math.floor((Math.random() * 21) + 1);
-console.log(pcNumber);
+console.log("il numero del pc", + pcNumber);
 
 //l'utente deve scegliere un numero a caso fra 1 e 10 e poi decidere se fermarsi
 //o proseguire
 var userArray=[];
 var control=false;
 var somma=0;
-var   i=0;
 //il programma chiede all'utente di inserire un numero fra 0 o 1, se
 //l'utente sceglie 1, il prog genera un num casuale fra 1 e 10;
-do{
 
+do{
   scelta=prompt("inserisci un numero per decidere cosa fare");
 
+
   if(scelta==1){
-  var userNumber=Math.floor((Math.random() * 10) + 1);
-  userArray.push(userNumber);
-  somma=somma + userArray[i];
-  i++;
+    var userNumber=Math.floor((Math.random() * 10) + 1);
+    somma+=userNumber;
+    console.log("il numero del utente", + userNumber);
 
-
-  alert("la somma dei numeri scelti dall'user è", + somma);
-}
-  else if (scelta == 0) {
+  }
+  else if (scelta==0) {
     control=true;
   }
   else{
-    alert("scelta sbagliata");
+    alert("scelta sbagliata, inserisci 0 o 1");
+    scelta=prompt("inserisci un numero per decidere cosa fare");
+
   }
+
 
 }while((control==false)&&(somma<22));
 
+
 if((somma<pcNumber)&&(somma<22)){
-  document.write("hai perso, somma = ", + somma);
+  document.write("hai perso, somma = ", +    somma);
 }
 else if((somma>pcNumber)){
   document.write("hai vinto, somma = ", + somma);
 }
+
 else if(somma==pcNumber){
-  document.write("PAREGGio")
-}
-
-
-
-function getSum(total, num) {
-    return total + num;
+  document.write("PAREGGio");
 }
